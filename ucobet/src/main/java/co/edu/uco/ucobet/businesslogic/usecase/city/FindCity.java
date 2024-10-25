448327504e724a6127ef8b8bfd29758183a1f9ab
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2024-10-24 23:36:50 -0500

    fix: correcion impl FindCity

--- ucobet/src/main/java/co/edu/uco/ucobet/businesslogic/usecase/city/FindCity.java
+++ ucobet/src/main/java/co/edu/uco/ucobet/businesslogic/usecase/city/FindCity.java
@@ -1,5 +1,10 @@
 package co.edu.uco.ucobet.businesslogic.usecase.city;
 
-public interface FindCity {
+import java.util.List;
+
+import co.edu.uco.ucobet.businesslogic.usecase.UseWithReturn;
+import co.edu.uco.ucobet.domain.CityDomain;
+
+public interface FindCity extends UseWithReturn<CityDomain, List<CityDomain>>{
 
 }
--- ucobet/src/main/java/co/edu/uco/ucobet/businesslogic/usecase/city/impl/FindCityImpl.java
+++ ucobet/src/main/java/co/edu/uco/ucobet/businesslogic/usecase/city/impl/FindCityImpl.java
@@ -8,7 +8,7 @@ import co.edu.uco.ucobet.domain.CityDomain;
 public final class FindCityImpl implements FindCity{
 
 	@Override
-	public List<CityDomain> execute(CityDomain data) {
+	public List<CityDomain> execute(final CityDomain data) {
 		// TODO Auto-generated method stub
 		return null;
 	}
